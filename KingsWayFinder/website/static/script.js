35d5f2913bf279cc33c60c0cc0c88c5f07cfb03d
Zach Hamilton <Zach.hamilton@kingsway.school.nz>
2026-08-01 19:37:30 +1200

    Made the app resize Fixed finderinput position bug

--- KingsWayFinder/website/static/script.js
+++ KingsWayFinder/website/static/script.js
@@ -17,43 +17,35 @@ const astroPolygon = document.getElementById("astro_polygon");
 const finderInput = document.getElementById("finder_input");
 
 
+// layout maps
+const sanfordLayoutMap = document.getElementById("sanford_layout_map");
+const allenLayoutMap = document.getElementById("allen_layout_map");
+const bolamLayoutMap = document.getElementById("bolam_layout_map");
+const ecLayoutMap = document.getElementById("ec_layout_map");
+const teKaingaLayoutMap = document.getElementById("te-kainga_layout_map");
+const artsBlockLayoutMap = document.getElementById("arts_block_layout_map");
+
+
+// layout maps back buttons
+const sanfordLayouMapBackButton = document.getElementById("sanford_layout_map_back_button");
+const allenLayoutMapBackButton = document.getElementById("allen_layout_map_back_button");
+const bolamLayoutMapBackButton = document.getElementById("bolam_layout_map_back_button");
+const ecLayoutMapBackButton = document.getElementById("ec_layout_map_back_button");
+const teKaingaLayoutMapBackButton = document.getElementById("te-kainga_layout_map_back_button");
+const artsBlockLayoutMapBackButton = document.getElementById("arts_block_layout_map_back_button");
+
+
+// const for changeing the positon of the finder input
+const mediaQuery1024px = window.matchMedia("(max-width: 1024px)");
+const contentDiv = document.querySelector(".content");
+// enables the code to put back the finder input to where it started
+const finderOriginalParent = finderInput.parentElement;
+const finderOriginalNextSibling = finderInput.nextElementSibling;
 
 
 
 
-// when activeated it will close the left sidebar and make the map take up the full screen. The width of the sidebar would get smaller and it will be translated to the right
-function closeLeftSidebar() {
-    if (leftSidePannelOpenButton){
-        leftSidePannelOpenButton.classList.remove("hidden");
-        leftSidePannelOpenButton.classList.add("hidden_animation");
-    }
 
-    if (leftSidebar){
-        leftSidebar.classList.remove("opening");
-        leftSidebar.classList.add("closing");
-    }
-}
-
-// when activeated it will open the left sidebar. the width of the sidebar will grow and it will be translated to the right
-function openLeftSidebar(){
-    if(leftSidePannelOpenButton){
-        leftSidePannelOpenButton.classList.remove("hidden_animation");
-        leftSidePannelOpenButton.classList.add("hidden");
-    }
-
-    if(leftSidebar){
-        leftSidebar.classList.remove("closing");
-        leftSidebar.classList.add("opening");
-    }
-    
-}
-
-
-// when the left sidebar close button it click it wil run the closeLeftSidebar which will close the leftsidebar and make the map take up the whole screen
-leftSidePannelCloseButton.addEventListener("click", closeLeftSidebar);
-
-// when the left sidebar open button it click it wil run the LeftSidebar which will open the leftsidebar
-leftSidePannelOpenButton.addEventListener("click", openLeftSidebar);
 
 
 
@@ -173,25 +165,42 @@ finderInput.addEventListener("input", checkForHighlights);
 
 
 
+// when activeated it will close the left sidebar and make the map take up the full screen. The width of the sidebar would get smaller and it will be translated to the right
+function closeLeftSidebar() {
+    if (leftSidePannelOpenButton){
+        leftSidePannelOpenButton.classList.remove("hidden");
+        leftSidePannelOpenButton.classList.add("hidden_animation");
+    }
+
+    if (leftSidebar){
+        leftSidebar.classList.remove("opening");
+        leftSidebar.classList.add("closing");
+    }
+}
+
+// when activeated it will open the left sidebar. the width of the sidebar will grow and it will be translated to the right
+function openLeftSidebar(){
+    if(leftSidePannelOpenButton){
+        leftSidePannelOpenButton.classList.remove("hidden_animation");
+        leftSidePannelOpenButton.classList.add("hidden");
+    }
+
+    if(leftSidebar){
+        leftSidebar.classList.remove("closing");
+        leftSidebar.classList.add("opening");
+    }
+    
+}
+
+
+// when the left sidebar close button it click it wil run the closeLeftSidebar which will close the leftsidebar and make the map take up the whole screen
+leftSidePannelCloseButton.addEventListener("click", closeLeftSidebar);
+
+// when the left sidebar open button it click it wil run the LeftSidebar which will open the leftsidebar
+leftSidePannelOpenButton.addEventListener("click", openLeftSidebar);
 
 
 
-// layout maps
-const sanfordLayoutMap = document.getElementById("sanford_layout_map");
-const allenLayoutMap = document.getElementById("allen_layout_map");
-const bolamLayoutMap = document.getElementById("bolam_layout_map");
-const ecLayoutMap = document.getElementById("ec_layout_map");
-const teKaingaLayoutMap = document.getElementById("te-kainga_layout_map");
-const artsBlockLayoutMap = document.getElementById("arts_block_layout_map");
-
-
-// layout maps back buttons
-const sanfordLayouMapBackButton = document.getElementById("sanford_layout_map_back_button");
-const allenLayoutMapBackButton = document.getElementById("allen_layout_map_back_button");
-const bolamLayoutMapBackButton = document.getElementById("bolam_layout_map_back_button");
-const ecLayoutMapBackButton = document.getElementById("ec_layout_map_back_button");
-const teKaingaLayoutMapBackButton = document.getElementById("te-kainga_layout_map_back_button");
-const artsBlockLayoutMapBackButton = document.getElementById("arts_block_layout_map_back_button");
 
 
 // each function changes which maps is being shown.
@@ -282,6 +291,53 @@ function showArtsBlockLayoutMap(){
 
 artsPolygon.addEventListener("click", showArtsBlockLayoutMap);
 artsBlockLayoutMapBackButton.addEventListener("click", showArtsBlockLayoutMap);
+
+
+
+
+
+
+// function fo move the finder input
+
+function changeInputPosition(screenwidth) {
+  
+    // if the screen is 1024 pc or less tis runs
+    if (screenwidth.matches) {
+        
+        // move the finder input to the top of the page
+        contentDiv.prepend(finderInput);
+
+
+    } else { // if the screen is larger than 1024px then the input goes back to its original position
+            if (finderOriginalNextSibling){
+
+                finderOriginalParent.insertBefore( finderInput, finderOriginalNextSibling );
+            } else{
+                // fail safe in case there isn't in element behind the finder input
+                finderOriginalParent.append( finderInput);
+            }
+        }
+}
+
+
+// if the screen changegs size the changeInputPosition function is run
+mediaQuery1024px.addEventListener("change", changeInputPosition);
+
+// runs it at the start so that if the screen is less then 1024px to begin with the finder is in the right place.
+changeInputPosition(mediaQuery1024px);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
